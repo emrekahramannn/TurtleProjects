255a8169c189b3c53774f610f2eaa69290f0a167
Emre Kahraman <emrekahrman@gmail.com>
2023-05-23 16:44:39 +0300

    Drawing board updated

--- drawingboard.py
+++ drawingboard.py
@@ -1,28 +1,47 @@
+# import module
 import turtle
 
+# set the screen
 drawing_board = turtle.Screen()
 drawing_board.bgcolor("light blue")
 drawing_board.title("Drawing Board for Kids")
 
+
+# create the turtle
 turtle_instance = turtle.Turtle("turtle")
+
+
+# define functions for keys
 
 def go_forward():
     turtle_instance.forward(50)
 
-def go_backward():
-    turtle_instance.backward(50)
+def rotate_right_angle():
+    turtle_instance.setheading(turtle_instance.heading() + 10)
+    # turtle_instance.right(10)
 
-def go_up():
-    turtle_instance.left(90)
+def rotate_left_angle():
+    turtle_instance.setheading(turtle_instance.heading() - 10)
+    # turtle_instance.left(10)
+    
+def clear_screen():
+    turtle_instance.clear()
 
-def go_down():
-    turtle_instance.right(90)
+def turtle_go_home():
+    turtle_instance.penup()
+    turtle_instance.home()
+    turtle_instance.pendown()
 
 
+# connect screen - key
 drawing_board.listen()
-drawing_board.onkey(go_forward, "Up")
-drawing_board.onkey(go_backward, "Down")
-drawing_board.onkey(go_up, "Right")
-drawing_board.onkey(go_down, "Left")
 
+# assign keys to defined functions
+drawing_board.onkey(fun=go_forward, key="space")
+drawing_board.onkey(fun=rotate_right_angle, key="Down")
+drawing_board.onkey(fun=rotate_left_angle, key="Up")
+drawing_board.onkey(fun=clear_screen, key="c")
+drawing_board.onkey(fun=turtle_go_home, key="h")
+
+# keep the screen open
 turtle.mainloop()
